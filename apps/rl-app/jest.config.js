c79aec58eb56f75ee0ba76e6e724460f1f32d320
Conrad Mercer <conrad.mercer@gmail.com>
2020-01-24 16:47:18 +1030

    RAD-473: Added default rl angular app

---
+++ apps/rl-app/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'rl-app',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/rl-app',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};
