ee7904b0b748e6e1cba921f9a5bb3805470684e0
Conrad Mercer <conrad.mercer@gmail.com>
2020-01-25 16:07:12 +1030

    RAD-476: Github build WORKING

--- apps/rl-app/src/app/app.component.ts
+++ apps/rl-app/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { EntityManager } from 'rad-ecs';
-import { Renderable } from 'libs/rl-ecs/src/lib/components/renderable.model';
-import { GridPos } from 'libs/rl-ecs/src/lib/components/position.model';
 
 @Component({
   selector: 'rad-root',
@@ -12,11 +10,4 @@ export class AppComponent {
   title = 'rl-app';
 
   em = new EntityManager();
-
-  constructor() {
-    this.em.createEntity(
-      new Renderable({ image: 'Elemental0-11.png', zOrder: 100 }),
-      new GridPos({ x: 3, y: 3, z: 3 })
-    );
-  }
 }
