12c8c87939c68f8bc4edc69eeed85f7a593e194a
Conrad Mercer <conrad.mercer@gmail.com>
2020-07-23 10:42:15 +0930

    Full update, angular, nx and libs

--- apps/rl-app/src/app/app.component.ts
+++ apps/rl-app/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { EntityManager } from 'rad-ecs';
+import { EntityManager, EntityId } from 'rad-ecs';
+import { Knowledge, GridPos, Renderable, KnowledgeMap } from '@rad/rl-ecs';
+import { ValueMap } from '@rad/rl-utils';
 
 @Component({
   selector: 'rad-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'rl-app';
 
   em = new EntityManager();
+
+  viewerId: EntityId;
+
+  constructor() {
+    const currentEntity = this.em.create(
+      new GridPos({ x: 1, y: 1, z: 1 }),
+      new Renderable({ image: 'Elemental0-11.png', zOrder: 1 })
+    ).id;
+    const current: KnowledgeMap = new ValueMap();
+    current.set(new GridPos({ x: 0, y: 0, z: 1 }), [currentEntity]);
+    this.viewerId = this.em.create(
+      new GridPos({ x: 1, y: 2, z: 1 }),
+      new Knowledge({ current, history: new ValueMap() })
+    ).id;
+  }
 }
