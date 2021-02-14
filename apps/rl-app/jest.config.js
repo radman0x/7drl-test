637c8d26bc1a3b2bac8b9b79529fa7de7c1ad49b
Conrad Mercer <conrad.mercer@gmail.com>
2021-02-14 15:44:13 +1030

    RAD-534: Update unit test config

--- apps/rl-app/jest.config.js
+++ apps/rl-app/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   preset: '../jest.preset.js',
   coverageDirectory: '../../coverage/apps/rl-app',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js',
-  ],
   displayName: 'rl-app',
 };
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
 module.exports = {
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  resolver: '@nrwl/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html'],
+  passWithNoTests: true,
   projects: [
-    '<rootDir>/libs/rl-ui',
     '<rootDir>/libs/rad-pixi',
+    '<rootDir>/libs/rl-applib',
     '<rootDir>/libs/rl-ecs',
+    '<rootDir>/libs/rl-procgen',
+    '<rootDir>/libs/rl-ui',
+    '<rootDir>/libs/rl-utils',
     '<rootDir>/',
   ],
 };
